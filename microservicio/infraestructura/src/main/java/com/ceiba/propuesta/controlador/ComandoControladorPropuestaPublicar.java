512cbc176d08b38cf1713663429b78958af4ef8e
Harvin Rengifo <harvin.rengifo@ceiba.com.co>
2021-08-24 03:10:18 -0500

    Fix: Actualización ControladorPublicarPropuesta

---
+++ microservicio/infraestructura/src/main/java/com/ceiba/propuesta/controlador/ComandoControladorPropuestaPublicar.java
@@ -0,0 +1,30 @@
+package com.ceiba.propuesta.controlador;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ceiba.propuesta.comando.manejador.ManejadorPublicarPropuesta;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/propuestas/{idPropuesta}/publicar")
+@Api(tags={"Controlador publicación de Propuesta de una Licitación"})
+public class ComandoControladorPropuestaPublicar {
+
+    private final ManejadorPublicarPropuesta manejadorPublicarPropuesta;
+
+    public ComandoControladorPropuestaPublicar(ManejadorPublicarPropuesta manejadorPublicarPropuesta) {
+        this.manejadorPublicarPropuesta = manejadorPublicarPropuesta;
+    }
+
+    @PutMapping
+    @ApiOperation("Publicar Propuesta")
+    public void ejecutar(@PathVariable Long idPropuesta) {
+        manejadorPublicarPropuesta.ejecutar(idPropuesta);
+    }
+    
+}
