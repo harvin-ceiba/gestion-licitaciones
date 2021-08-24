a6c78c175d3bc160fa275d77507905f6d7e50931
Harvin Rengifo <harvin.rengifo@ceiba.com.co>
2021-08-24 03:05:03 -0500

    Feat: Implementación servicio Publicación de Propuesta

---
+++ microservicio/dominio/src/main/java/com/ceiba/propuesta/servicio/ServicioPublicarPropuesta.java
@@ -0,0 +1,27 @@
+package com.ceiba.propuesta.servicio;
+
+import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
+import com.ceiba.propuesta.puerto.repositorio.RepositorioPropuesta;
+
+public class ServicioPublicarPropuesta {
+
+    private static final String LA_PROPUESTA_NO_EXISTE = "La propuesta que intenta publicar no existe";
+
+    private final RepositorioPropuesta repositorioPropuesta;
+
+    public ServicioPublicarPropuesta(RepositorioPropuesta repositorioPropuesta) {
+        this.repositorioPropuesta = repositorioPropuesta;
+    }
+
+    public void ejecutar(Long idPropuesta) {
+    	validarExistenciaPrevia(idPropuesta);
+        this.repositorioPropuesta.publicar(idPropuesta);
+    }
+
+    private void validarExistenciaPrevia(Long idPropuesta) {
+    	boolean existe = this.repositorioPropuesta.existe(idPropuesta);
+        if(!existe) {
+            throw new ExcepcionValorInvalido(LA_PROPUESTA_NO_EXISTE);
+        }
+    }
+}
